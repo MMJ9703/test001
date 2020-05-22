b89b8d111018704a2b3ebac3cd5da51a4cad9062
MMJ9703 <64726856+MMJ9703@users.noreply.github.com>
2020-05-22 15:34:35 +0800

    Add files via upload

--- t_function.py
+++ t_function.py
@@ -1,15 +1,14 @@
 import pandas as pd
 
-data=pd.read_csv('C:/Users/pc6/Desktop/寄云/1.csv',encoding='gbk')
+#data=pd.read_csv('C:/Users/pc6/Desktop/寄云/1.csv',encoding='gbk')
 
-x = data['x']
-y = data['y']
+#x = data['x']
+#y = data['y']
 
 
 def fun(data):
-    x = data['x']
-    y = data['y']
-    z = x + y
-    return z
+    
+    data['z'] = data['x']+data['y']
+    
+    return data
 
-z = fun(data)

ba1e5e805bd8d076d39ea495c666697e4d049219
MMJ9703 <64726856+MMJ9703@users.noreply.github.com>
2020-10-12 14:18:05 +0800

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,9 @@
-
+# 添加所需要的库
 import os
-
-from t_function import fun
-
+# 添加自己编写的算法
+from Import_data import mainf
+# from read_data import read_xml, read_http
+# Web应用程序设置
 from flask_executor import Executor
 import pandas as pd
 from threading import Thread
@@ -11,9 +12,13 @@ from sqlalchemy import create_engine
 
 
 from flask import Flask
+# from flask import request
+# from flask_cors import *
+
 application = Flask(__name__)
 executor = Executor(application)
-
+# 获取mysql环境变量
+# os.environ['MYSQL_URI'] = 'mysql+pymysql://root:123456@localhost:3306/test'
 env = os.environ
 # MYSQL_URI   mysql+pymysql://test:test@172.30.238.185:3306/test
 mysql_uri = env.get('MYSQL_URI')
@@ -22,30 +27,34 @@ sqlEngine = create_engine(mysql_uri, pool_recycle=3600)
 
 print ('=== mysql uri: ' + mysql_uri)
 
-# rest  api
+# rest  api（应用执行端口）
 @application.route('/')
 def hello():
-    executor.submit(threaded_task,'data')
-    return b'fun '
+    executor.submit(threaded_task)
+    return b'mainf '
 
 if __name__ == '__main__':
 
     application.run()
 
-def threaded_task(data):  
+def threaded_task():  
     try:
 
         print ('===== run task')
 
-        # load imbalance2  from  mysql 
-        data_1 = pd.read_sql("select * from  TABLE1 ", con=sqlEngine, index_col=None)
-
-        print ('data1 to dataframe')
-
-        data_2 = fun(data_1)
-
-        # dataframe to  mysql 
-        data_2.to_sql('TABLE2', con=sqlEngine, if_exists='append', index=False)
+        # 机组信息
+        ID = 150817080435211
+        Input_Time = '2020-6-20 00:00:00'
+        days = 1
+        # ID,Input_Time,days = read_xml()
+        # data = request.get_json()
+        # ID,Input_Time,days = read_http(data)
+        # 算法执行
+        status = mainf(ID, Input_Time, days, sqlEngine)
+        # data["param"]["status"] = status
+        print(statue)
+        # 将数据写入mysql数据库中，设置查询pd.to_sql()函数
+        
         
 
     except Exception as e:
